2e3523b6a11509479fe787969b306be22aba2321
Felipe Sánchez <2018.felipe.sanchez@gmail.com>
2021-02-07 18:06:54 -0300

    gettin back on this project (new repo cuz the other ones were all weird) same code tho

---
+++ script.js
@@ -0,0 +1,8 @@
+
+function ell1 (){
+    document.getElementById(('atv-class-ell')*3 *0.2) +
+    document.getElementById(('prova-mensal-ell')*2 *0.4) +
+    document.getElementById(('prova-trimestral-ell')*2 *0.4)
+        
+}
+
