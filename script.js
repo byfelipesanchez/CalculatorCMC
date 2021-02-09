a9daf9ebd29c60d3811b06669344f2dc51555ac8
Felipe Sánchez <2018.felipe.sanchez@gmail.com>
2021-02-09 17:21:45 -0300

    Update script.js
    gotta figure out the calculation prob and turn the function into the subjects and areas (mensal/tri...)

--- script.js
+++ script.js
@@ -20,6 +20,68 @@ function ell1()
     document.getElementById("test").innerHTML = ellac1;
 }
 
+function ell1() 
+{
+    test1 = document.getElementById("atv-classe-ell-1").value;
+    test2 = document.getElementById("atv-classe-ell-2").value;
+    test3 = document.getElementById("atv-classe-ell-3").value;
+    ellac = (test1 + test2 + test3)
+    ellac1 = ellac/3
+
+    test4 = document.getElementById("prova-mensal-ell-1").value;
+    test5 = document.getElementById("prova-mensal-ell-2").value;
+    ellmensal = (test4 + test5)/2
+    ellmensal1 = ellmensal*0.4
+
+    test6 = document.getElementById("prova-trimestral-ell-1").value;
+    test7 = document.getElementById("prova-trimestral-ell-2").value;
+    elltri = (test6 + test7)/2
+    elltri1 = elltri*0.4
+
+    document.getElementById("test").innerHTML = ellac1;
+}
+
+function ell1() 
+{
+    test1 = document.getElementById("atv-classe-ell-1").value;
+    test2 = document.getElementById("atv-classe-ell-2").value;
+    test3 = document.getElementById("atv-classe-ell-3").value;
+    ellac = (test1 + test2 + test3)
+    ellac1 = ellac/3
+
+    test4 = document.getElementById("prova-mensal-ell-1").value;
+    test5 = document.getElementById("prova-mensal-ell-2").value;
+    ellmensal = (test4 + test5)/2
+    ellmensal1 = ellmensal*0.4
+
+    test6 = document.getElementById("prova-trimestral-ell-1").value;
+    test7 = document.getElementById("prova-trimestral-ell-2").value;
+    elltri = (test6 + test7)/2
+    elltri1 = elltri*0.4
+
+    document.getElementById("test").innerHTML = ellac1;
+}
+
+function ell1() 
+{
+    test1 = document.getElementById("atv-classe-ell-1").value;
+    test2 = document.getElementById("atv-classe-ell-2").value;
+    test3 = document.getElementById("atv-classe-ell-3").value;
+    ellac = (test1 + test2 + test3)
+    ellac1 = ellac/3
+
+    test4 = document.getElementById("prova-mensal-ell-1").value;
+    test5 = document.getElementById("prova-mensal-ell-2").value;
+    ellmensal = (test4 + test5)/2
+    ellmensal1 = ellmensal*0.4
+
+    test6 = document.getElementById("prova-trimestral-ell-1").value;
+    test7 = document.getElementById("prova-trimestral-ell-2").value;
+    elltri = (test6 + test7)/2
+    elltri1 = elltri*0.4
+
+    document.getElementById("test").innerHTML = ellac1;
+}
 
 
 // document.getElementById("test").innerHTML = test1 * test2;
